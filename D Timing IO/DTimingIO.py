b954f12990363db5dbc0f152b450540fb7e97b38
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-08-04 20:36:11 +0700

    print used time

--- D Timing IO/DTimingIO.py
+++ D Timing IO/DTimingIO.py
@@ -9,3 +9,6 @@ noIoEnd = time.time()
 ioStart = time.time()
 [print(i+1) for i in range(NO_OF_TIMES)]
 ioEnd = time.time()
+
+print(f"Time used w/o IO: {round(noIoEnd-noIoStart, 7)}ms")
+print(f"Time used w/  IO: {round(ioEnd-ioStart, 7)}ms")
