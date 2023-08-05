3f1bf67eca129b4ced0f291aea6a4a3061b14533
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-08-05 20:31:29 +0700

    print pointer

--- B Memory Management/CMemoryManagement.c
+++ B Memory Management/CMemoryManagement.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int MAX_N = 100000;
 int* heapPointer[100001];
 int* stackPointer[100001];
 
-
 void mallocNTimes(int n);
 
-
-int main(void)
+int main(void)  
 {
+    mallocNTimes(MAX_N);
+
+    printf("heap start: %x\n", heapPointer[0]);
+    printf("heap stop: %x\n\n", heapPointer[MAX_N]);
+    printf("stack start: %x\n", stackPointer[0]);
+    printf("stack stop: %x\n\n", stackPointer[MAX_N]);
+    printf("global variables 1: %x\n", MAX_N);
+    printf("global variables 2: %x\n", heapPointer);
+    printf("global variables 3: %x\n", stackPointer);
 
     return 0;
 }
