cb8b49b73b299816a5f32ed54dec0017b893e301
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-08-08 09:33:07 +0700

    change print order

--- B Memory Management/CMemoryManagement.c
+++ B Memory Management/CMemoryManagement.c
@@ -12,12 +12,12 @@ int main(void)
     mallocNTimes(0);
 
     printf("stack start: %p\n", stackPointer[0]);
-    printf("stack stop : %p\n\n", stackPointer[MAX_N-1]);
-    printf("heap stop  : %p\n\n", heapPointer[MAX_N-1]);
+    printf("stack stop : %p\n", stackPointer[MAX_N-1]);
+    printf("heap stop  : %p\n", heapPointer[MAX_N-1]);
     printf("heap start : %p\n", heapPointer[0]);
-    printf("global variables 1: %p\n", &MAX_N);
-    printf("global variables 2: %p\n", heapPointer);
     printf("global variables 3: %p\n", stackPointer);
+    printf("global variables 2: %p\n", heapPointer);
+    printf("global variables 1: %p\n", &MAX_N);
 
     return 0;
 }
