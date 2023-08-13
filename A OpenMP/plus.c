68dc7fa5352af6e3e0b028ad31af8e212b5b519d
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-08-13 15:53:44 +0700

    get input

--- A OpenMP/plus.c
+++ A OpenMP/plus.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
 
 int main(void)
 {
+    int sumThread[2];
+    int times, i;
+    int* input;
+
+    scanf("%d\n", &times);
+    input = (int*)malloc(sizeof(int) * times);
+    
+    for (i=0; i<times; i++)
+    {
+        scanf("%d", input[i]);
+    }
+    for (i=0; i<times; i++)
+    {
+        printf("%d", input[i]);
+    }
 
 
     return 0;
